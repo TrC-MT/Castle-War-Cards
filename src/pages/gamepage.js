b0097786a7de5267d7598d1d38fd8a54c68277ed
Travis Cook <travis.cook5583@stu.mtec.edu>
2023-04-25 15:11:06 -0600

    Player resources should be able to update.

--- src/pages/gamepage.js
+++ src/pages/gamepage.js
@@ -5,6 +5,51 @@ import ResourceStats from "../components/stats/resources";
 
 export default function GamePage(){
 
+    let players = {
+        one: {
+            name: 'Uno',
+            resources: {
+                build: {
+                    helpers: 2,
+                    currency: 4
+                },
+                attack: {
+                    helpers: 2,
+                    currency: 4
+                },
+                magic: {
+                    helpers: 2,
+                    currency: 4
+                },
+            },
+            constructs: {
+                castle: 40,
+                fence: 20,
+            }
+        },
+        two: {
+            name: 'Dos',
+            resources: {
+                build: {
+                    helpers: 2,
+                    currency: 6
+                },
+                attack: {
+                    helpers: 3,
+                    currency: 3
+                },
+                magic: {
+                    helpers: 1,
+                    currency: 2
+                },
+            },
+            constructs: {
+                castle: 40,
+                fence: 20,
+            }
+        }
+    }
+
     function randnum(max){
         let num = Math.floor(Math.random() * (max -1));
         return num;
@@ -15,9 +60,9 @@ export default function GamePage(){
         <>
             <div id="gamepage-container">
                 <div id="left-player-container" class="player-container">
-                    <ResourceStats render={{type: {helper: {name: 'Build', amount: 2}, currency: {name: 'Bricks', amount: 8}},  class: "build"}}></ResourceStats>
-                    <ResourceStats render={{type: {helper: {name: 'Soldiers', amount: 3}, currency: {name: 'Weapons', amount: 6}}, class: "attack"}}></ResourceStats>
-                    <ResourceStats render={{type: {helper: {name: 'Magicians', amount: 1}, currency: {name: 'Crystals', amount: 3}}, class: "magic"}}></ResourceStats>
+                    <ResourceStats render={{type: {helper: {name: 'Build', amount: players.one.resources.build.helpers}, currency: {name: 'Bricks', amount: players.one.resources.build.currency}},  class: "build"}}></ResourceStats>
+                    <ResourceStats render={{type: {helper: {name: 'Soldiers', amount: players.one.resources.attack.helpers}, currency: {name: 'Weapons', amount: players.one.resources.attack.currency}}, class: "attack"}}></ResourceStats>
+                    <ResourceStats render={{type: {helper: {name: 'Magicians', amount: players.one.resources.magic.helpers}, currency: {name: 'Crystals', amount: players.one.resources.magic.currency}}, class: "magic"}}></ResourceStats>
                 </div>
                 <div id="middle-box">
                     <div id="field-container">
@@ -32,12 +77,54 @@ export default function GamePage(){
                     </div>
                 </div>
                 <div id="right-player-container" class="player-container">
-                <ResourceStats render={{type: {helper: {name: 'Build', amount: 2}, currency: {name: 'Bricks', amount: 10}},  class: "build"}}></ResourceStats>
-                <ResourceStats render={{type: {helper: {name: 'Soldiers', amount: 2}, currency: {name: 'Weapons', amount: 4}}, class: "attack"}}></ResourceStats>
-                <ResourceStats render={{type: {helper: {name: 'Magicians', amount: 2}, currency: {name: 'Crystals', amount: 6}}, class: "magic"}}></ResourceStats>
+                <ResourceStats render={{type: {helper: {name: 'Build', amount: players.two.resources.build.helpers}, currency: {name: 'Bricks', amount: players.two.resources.build.currency}},  class: "build"}}></ResourceStats>
+                <ResourceStats render={{type: {helper: {name: 'Soldiers', amount: players.two.resources.attack.helpers}, currency: {name: 'Weapons', amount: players.two.resources.attack.currency}}, class: "attack"}}></ResourceStats>
+                <ResourceStats render={{type: {helper: {name: 'Magicians', amount: players.two.resources.magic.helpers}, currency: {name: 'Crystals', amount: players.two.resources.magic.currency}}, class: "magic"}}></ResourceStats>
                 </div>
             </div>
 
         </>
     )
+
+    //------------------------------
+    let turn_count = 0;
+    let player_turn = 'one';
+    function turn(){
+        turn_count += 1;
+        if(turn_count %2 == 0){
+            player_turn = 'two';
+        }
+        else{
+            player_turn = 'one';
+        }
+
+        //update player resources
+        if(player_turn == 'one'){
+            players.one.resources.build.currency += players.one.resources.build.helpers
+            players.one.resources.attack.currency += players.one.resources.attack.helpers
+            players.one.resources.magic.currency += players.one.resources.magic.helpers
+
+        }
+        else if(player_turn == 'two'){
+            players.two.resources.build.currency += players.two.resources.build.helpers
+            players.two.resources.attack.currency += players.two.resources.attack.helpers
+            players.two.resources.magic.currency += players.two.resources.magic.helpers
+        }
+        else{
+            console.log('player_turn malfunction')
+        }
+        // show correct player cards
+        
+        // let player pick a card
+
+        //play the card, get a new one
+
+        //update players resources //make sure none of them are below 1
+
+        // if neither player is out, then recall turn()
+    }
+
+    
+
+    //---------------------------
 }
